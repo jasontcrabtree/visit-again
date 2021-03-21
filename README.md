a4561973f7d84b2a5dfa4219294f4b036348d3fa
jasontcrabtree <jasontcrabtree@gmail.com>
2021-03-21 16:43:35 +1300

    refactor from sales-utility-app to base next starter

--- README.md
+++ README.md
@@ -1,14 +1,3 @@
-
-# Total Project Time
-
-V-1 Sales Email App done in 9 hours
+# Next JS Base Template
 
 `npm run dev`
-
-Hosted on Netlify
-
-Tech:
-
-- Next JS (for SSG)
-- Chakra UI (for Styling + Modal Components)
-- React Hooks (Input + Text Interpolation)
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,22 +1,17 @@
-import { Box, ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import Head from 'next/head';
-
-import customTheme from '../utils/theme';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
         <title>Sales Emails | Lodestone Studio</title>
-        <link rel="shortcut icon" href="/star.svg" />
+        <link rel="shortcut icon" href="/refresh-cw.svg" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <ColorModeScript initialColorMode={customTheme.config.initialColorMode} />
-      <ChakraProvider resetCSS theme={customTheme}>
-        <Box w="100%" h="100%" p={[4, 8, 24]} pt={[4, 8, 12]}>
+        <div>
           <Component {...pageProps} />
-        </Box>
-      </ChakraProvider>
+        </div>
+
     </>
   );
 }

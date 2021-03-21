48ede35d587c4f441863cb153cf81aec72fb2892
jasontcrabtree <jasontcrabtree@gmail.com>
2021-03-21 17:13:27 +1300

    refine base

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,17 +1,18 @@
 import Head from 'next/head';
+import '../styles/reset.css';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Sales Emails | Lodestone Studio</title>
-        <link rel="shortcut icon" href="/refresh-cw.svg" />
+        <title>NextJS Base | Lodestone Studio</title>
+        <link rel="shortcut icon" href="/Favicon.svg" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-        <div>
-          <Component {...pageProps} />
-        </div>
-
+      <div>
+        <Component {...pageProps} />
+      </div>
     </>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,8 @@
 export default function Home() {
-  const page = "Home"
-
   return (
     <main>
       <h1>Tiny JS Starter</h1>
       <p>This is a Next JS template setup with files how I prefer</p>
     </main>
-  )
+  );
 }

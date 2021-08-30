81f36cf2c955e869ac339fbd402c7a766fc298d7
jasontcrabtree <jasontcrabtree@gmail.com>
2021-08-30 22:31:49 +1200

    start new next ts base

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,9 @@ import Head from 'next/head';
 import '../styles/reset.css';
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
+const hello = 'string';
+
+function MyApp({ Component, pageProps }): any {
   return (
     <>
       <Head>
--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+const hello = 'string';
+
 export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' });
+  return res.status(200).json({ name: 'John Doe' });
 };

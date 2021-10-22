bb4e19630f8846d91044f7bfa0089fdd6726db03
jasontcrabtree <jasontcrabtree@gmail.com>
2021-10-22 23:56:08 +1300

    setup simplest possible demo of prisma + next + ts + sqlite

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,27 @@
-export default function Home() {
+import prisma from '../lib/prisma';
+
+type Props = {
+  props: any;
+  authors: object;
+};
+
+export default function Home(props: Props): JSX.Element {
+  const data = props.authors[0];
+
   return (
     <main>
-      <h1>Tiny JS Starter</h1>
+      <h1>Tiny JS Starter{data ? <span>, Hello {data.name}!</span> : null}</h1>
       <p>This is a Next JS template setup with files how I prefer</p>
     </main>
   );
 }
+
+export const getServerSideProps = async ({ req }) => {
+  const authors = await prisma.author.findMany({
+    select: { id: true, name: true },
+  });
+
+  console.log(authors);
+
+  return { props: { authors } };
+};

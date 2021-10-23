fc637ac96938e04544ceecbf05daca95fadee288
jasontcrabtree <jasontcrabtree@gmail.com>
2021-10-23 14:44:19 +1300

    add in extremely basic obscured content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import prisma from '../lib/prisma';
 
 type Props = {
@@ -6,12 +7,62 @@ type Props = {
 };
 
 export default function Home(props: Props): JSX.Element {
+  const [unsafePrivate, setUnsafePrivate] = useState(false);
+  const [secretWord, setSecretWord] = useState('');
+
   const data = props.authors[0];
+
+  function handleFormSubmission(e) {
+    e.preventDefault();
+    /* This is extremely unsafe and should not be used as an actual security method.
+    It is used to hide Prisma data in the base template. */
+    if (secretWord === '123') {
+      setUnsafePrivate(true);
+    } else {
+      setSecretWord('');
+    }
+  }
+
+  function revertSecret() {
+    setUnsafePrivate(false);
+    setSecretWord('');
+  }
 
   return (
     <main>
-      <h1>Tiny JS Starter{data ? <span>, Hello {data.name}!</span> : null}</h1>
-      <p>This is a Next JS template setup with files how I prefer</p>
+      <h1>Tiny JS Starter</h1>
+      <p>
+        This is a Next JS, TS and Prisma + SQLite template setup with files in a
+        nice format
+      </p>
+
+      {unsafePrivate && (
+        <div>
+          {data && <h2>Hello {data.name}!</h2>}
+          <p>
+            This is controlled by state and hides some content but shouldn't be
+            trusted for private content.
+          </p>
+        </div>
+      )}
+
+      <form onSubmit={handleFormSubmission}>
+        <label htmlFor="secretWord">
+          Code?
+          <input
+            type="text"
+            name="secretWord"
+            autoComplete="off"
+            value={secretWord}
+            onChange={e => setSecretWord(e.target.value)}
+          />
+        </label>
+        <input type="submit" value="Hack-in" />
+      </form>
+
+      <button type="button" onClick={revertSecret}>
+        Revert
+      </button>
     </main>
   );
 }
